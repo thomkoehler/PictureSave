b3ddeb71e6d267f06dfce0793b349f495c9ced92
Koehler <thomas.koehler@realtech.com>
2017-02-06 16:26:34 +0100

    PictureSave

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Files
 import Graphics.HsExif
 import System.FilePath.Windows
+import Data.Time.LocalTime
+import Data.Time.Format
 
 
 jpgExt :: String
@@ -13,11 +15,18 @@ main :: IO ()
 main = copyJpgs "./Temp/src" "./Temp/target"
 
 
+mbLocalTimeToFilename :: Maybe LocalTime -> String -> String
+mbLocalTimeToFilename Nothing oldFileName = oldFileName
+mbLocalTimeToFilename (Just lt) _ = formatTime defaultTimeLocale "%0Y" lt
+
+
 copyJpgs :: FilePath -> FilePath -> IO ()
 copyJpgs srcDir targetDir = do
    srcFiles <- listDir jpgExt srcDir
    srcFileExifInfos <- mapM exifTimeOriginal srcFiles
    let srcFileNames = map takeFileName srcFiles
+
+   print $ zipWith mbLocalTimeToFilename srcFileExifInfos srcFileNames
 
    targetFiles <- listDir jpgExt targetDir
    let targetFileNames = map takeFileName targetFiles
